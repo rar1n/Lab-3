315808eee38011579fee77defd0530f9d354c3c3
rar1n <bolbat.ilya13@gmail.com>
2025-04-09 21:09:47 +0300

    Л.Р.3 Завдання 2 (2 варіант)
    Л.Р.3 Завдання 2 (2 варіант)

---
+++ Л.Р.3 Завдання 2 (2 варіант)/Л.Р.3 Завдання 2 (Варіант 2)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Moneta
+{
+    public int Nominal { get; set; } // Номінал монети
+    public int Kilkist { get; set; } // Кількість монет
+    public Moneta(int nominal, int kilkist) 
+    {
+        Nominal = nominal; 
+        Kilkist = kilkist; 
+    }
+    public string MonetaInfo() // Інформація про монету
+    {
+        return $"Номiнал монети: {Nominal} грн, Кiлькiсть монет: {Kilkist}";  
+    }
+    public int SumaMonet() // Обчислення суми монет
+    {
+        return Nominal * Kilkist;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введiть номiнал монети (1, 2, 5, 10 i т.д.): "); // Введення номіналу монети
+        int nominal = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введiть кiлькiсть монет: "); // Введення кількості монет
+        int kilkist = Convert.ToInt32(Console.ReadLine());
+        Moneta moneta = new Moneta(nominal, kilkist); 
+        Console.WriteLine(moneta.MonetaInfo()); 
+        int smonet = moneta.SumaMonet(); 
+        Console.WriteLine($"Сума монет: {smonet} грн");
+    }
+}
